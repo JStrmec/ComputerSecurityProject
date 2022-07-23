0156a92cee1f3b246782dbafc0065c9c91cb3189
Jocelyn Strmec <strj_vienna@yahoo.com>
2022-07-22 17:43:50 -0700

    last run through

--- encryption_algs/encryption_alg4.py
+++ encryption_algs/encryption_alg4.py
@@ -1,1 +1,76 @@
-#ne-ver mind
+def to_bin(data):
+    """Convert `data` to binary format as string"""
+    if isinstance(data, str):
+        return ''.join([ format(ord(i), "08b") for i in data ])
+    elif isinstance(data, bytes) or isinstance(data, np.ndarray):
+        return [ format(i, "08b") for i in data ]
+    elif isinstance(data, int) or isinstance(data, np.uint8):
+        return format(data, "08b")
+    else:
+        raise TypeError("Type not supported.")
+
+def encode(file_name, secret_data):
+    secret_data += "====="
+    # convert data to binary
+    binary_secret_data = to_bin(secret_data)  
+    # size of data to hide
+    data_len = len(binary_secret_data)
+        
+    file1 = open(f"unencrypted_files/{file_name}.txt","r") 
+    file2 = open(f"encrypted_files/{file_name}_encoded.txt","w+") 
+    
+    data_index = 0
+    while True:     
+        data = file1.readline()
+        if(data_index<data_len):
+            if(binary_secret_data[data_index]=='1'):  # replace the 'bit'
+                data=data[0].lower() + data[1:]  # convert first letter to lowercase
+        file2.write(data)
+        data_index+=1
+        if not data:
+            break
+    
+def decode(file_name):
+     crypto =''
+     file2 = open(f"encrypted_files/{file_name}_encoded.txt","r") 
+     while True:
+        data = file2.readline()
+        if not data:
+            break
+        # decodes lower and upper case letters of each line to secret message
+        if(data[0].isupper()):
+            crypto=crypto+'0'
+        else:
+            crypto=crypto+'1'
+         
+     #print("Encrypted Code Bits:") 
+     #print(crypto)
+     data_lencr = len(crypto)  
+     secret=''
+     decoded_data = ""
+     for byte in crypto:
+        decoded_data += chr(int(byte, 2))
+        if decoded_data[-5:] == "=====":
+              break
+    #  for i in range(0, data_lencr,7):
+    #      num=0
+         
+    #      num=num+int(crypto[i+0])*64
+    #      if(i+1<data_lencr):
+    #          num=num+int(crypto[i+1])*32
+    #      if(i+2<data_lencr):
+    #          num=num+int(crypto[i+2])*16
+    #      if(i+3<data_lencr):
+    #          num=num+int(crypto[i+3])*8
+    #      if(i+4<data_lencr):
+    #          num=num+int(crypto[i+4])*4
+    #      if(i+5<data_lencr):
+    #          num=num+int(crypto[i+5])*2
+    #      if(i+6<data_lencr):
+    #          num=num+int(crypto[i+6])*1
+    #      if(num>64):
+    #          charac = chr(num)
+    #          secret=secret+charac
+    #      if(num==0):
+    #          break;  
+     return decoded_data
--- main.py
+++ main.py
@@ -9,12 +9,16 @@ import cv2
 from datetime import datetime
 from encryption_algs import encryption_alg2 as encrypt1
 from encryption_algs import encryption_alg3 as encrypt2
+from encryption_algs import encryption_alg4 as encrypt3
 
 images_dict ={0:'unencrypted_files/clouds.webp', 1:'unencrypted_files/mountains.jpeg',2:'unencrypted_files/poppy_field.webp',3:'unencrypted_files/sunset.jpeg',4:'unencrypted_files/walk_central_park.webp'}
-auto_secret_msg = "It\\'s sandwich day. Every Thursday I give Pudge the Fish a peanut butter sandwich. But, today we were out of peanut butter. I asked my sister what to give him and she said a tuna sandwich. I can\\’t give Pudge tuna! Do you know what tuna is? It's a fish!"
+text_dict ={0:'text_file', 1:'text_file1',2:'text_file2',3:'text_file3',4:'text_file4'}
+auto_secret_msg = "It\\'s sandwich day.b I can\\’t give Pudge tuna! Do you know what tuna is? It's a fish!"
 
 
 def main():
+    print("\n\nWould you like to encode your message in an ( a ) image or ( b ) a text file? \nSelect an option by inputting corresponding letter")
+    message_type = input()
     print('Input a single integer between 0-4.')
     file_num = input()
     print("Type in your secret message to encrypt: ")
@@ -25,33 +29,47 @@ def main():
     filename, ext = output_file.split(".")
     _, file= filename.split("/")
 
-    # if else switches which algo is used based on current minute's eveness
-    if(datetime.now().minute%2 == 0):
-        # encodes image
-        encoded_image = encrypt2.encode(output_file, secret)
-        # saves image
-        cv2.imwrite(f"encrypted_files/{file}_encoded.{ext}", encoded_image)
-        
-        print("Would you like to decode the data?  y or n")
-        affirmative = input()
-        if affirmative.lower().strip() == 'y':
-            # decodes image
-            decoded_data = encrypt2.decode(f"encrypted_files/{file}_encoded.{ext}")   
-            # prints message
-            print("Secret message is:", decoded_data)
+    if(message_type.lower() == "a"):
+        # if else switches which algo is used based on current minute's eveness
+        if(datetime.now().minute%2 == 0):
+            # encodes image
+            encoded_image = encrypt2.encode(output_file, secret)
+            # saves image
+            cv2.imwrite(f"encrypted_files/{file}_encoded.{ext}", encoded_image)
+            
+            print("Would you like to decode the data?  y or n")
+            affirmative = input()
+            if affirmative.lower().strip() == 'y':
+                # decodes image
+                decoded_data = encrypt2.decode(f"encrypted_files/{file}_encoded.{ext}")   
+                # prints message
+                print("Secret message is:", decoded_data)
+        else:
+            # encodes image
+            encoded_image = encrypt1.encode(output_file, secret)
+            # saves image
+            cv2.imwrite(f"encrypted_files/{file}_encoded.{ext}", encoded_image)
+
+            print("Would you like to decode the data?  y or n")
+            affirmative = input()
+            if affirmative.lower().strip() == 'y':
+                # decodes images
+                decoded_data = encrypt1.decode(f"encrypted_files/{file}_encoded.{ext}")
+                # prints message
+                print("Secret message is:", decoded_data)
     else:
+        file = text_dict[int(file_num.strip())]
         # encodes image
-        encoded_image = encrypt1.encode(output_file, secret)
-        # saves image
-        cv2.imwrite(f"encrypted_files/{file}_encoded.{ext}", encoded_image)
+        encrypt3.encode(file, secret)
 
         print("Would you like to decode the data?  y or n")
         affirmative = input()
         if affirmative.lower().strip() == 'y':
             # decodes images
-            decoded_data = encrypt1.decode(f"encrypted_files/{file}_encoded.{ext}")
+            decoded_data = encrypt3.decode(file)
             # prints message
             print("Secret message is:", decoded_data)
+
     print("Goodbye!")
 
 main()   
